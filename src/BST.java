1cd78ba906e1a72a7edc62ee706fb7dd84c43cf1
Lei Zhang <leiz123321@gmail.com>
2026-09-22 19:25:57 -0400

    implemented isEmpty on class BST

--- src/BST.java
+++ src/BST.java
@@ -29,7 +29,7 @@ public class BST {
 
 
     public boolean isEmpty() {
-        return false; // TODO implement me!
+        return this.root == null;
     }
 
     public boolean contains(int item) {
@@ -47,7 +47,9 @@ public class BST {
 
 
     public void insert(int item) {
+        if (isEmpty()) {
 
+        }
     }
 
 
