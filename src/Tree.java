a7b941eb32a78836c9544abb247538acf9685c99
Pamela Clements <pam.is.cool123@gmail.com>
2026-09-22 19:58:18 -0400

    Extend Tree class from MultiSet and add placeholder method implementations

--- src/Tree.java
+++ src/Tree.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Tree {
+public class Tree extends MultiSet {
     // We recommend attempting this class last, as it hasn't been scaffolded for your team.
     // Even if your team doesn't have time to implement this class, it is a useful exercise
     // to think about how you might split up the work to get the Tree and TreeMultiSet
@@ -14,4 +14,38 @@ public class Tree {
         this.subtrees = Objects.requireNonNullElseGet(subtrees, ArrayList::new);
     }
 
+    public Tree() {
+        this(0, new ArrayList<>());
+    }
+
+
+    @Override
+    void add(int item) {
+
+    }
+
+    @Override
+    void remove(int item) {
+
+    }
+
+    @Override
+    boolean contains(int item) {
+        return false;
+    }
+
+    @Override
+    boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    int count(int item) {
+        return 0;
+    }
+
+    @Override
+    int size() {
+        return 0;
+    }
 }
